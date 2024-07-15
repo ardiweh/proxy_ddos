23008e982e07bb4705dc85053acb1f9824853f5a
ardiweh <113125338+ardiweh@users.noreply.github.com>
2024-07-15 18:54:13 +0700

    Update proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -1,32 +1,29 @@
 from scapy.all import sniff, send, IP, TCP, UDP, wrpcap
 
 # Define ports mapping for forwarding the packets
-TARGET_PORT_MAPPING = {
-    80: 8080,   # HTTP
-    443: 8443,  # HTTPS
-    53: 8053,   # DNS
-    22: 8022,   # SSH
-    21: 8021,   # FTP
-    25: 8025,   # SMTP
-    123: 8123,  # NTP
-    389: 8389,  # LDAP
-    1900: 8190, # SSDP
-    1433: 8433, # MSSQL
-    3389: 8389  # RDP
+TARGET_PORT = {
+    80,   # HTTP
+    443,  # HTTPS
+    53,   # DNS
+    22,   # SSH
+    21,   # FTP
+    25,   # SMTP
+    123,  # NTP
+    389,  # LDAP
+    1900, # SSDP
+    1433, # MSSQL
+    3389  # RDP
 }
 
-# Define the client and server IP address ranges or specific IPs
-# CLIENT_IP_RANGE = '192.168.1.0/24'
-SERVER_IP_RANGE = '192.168.1.25'
+# Define the server IP address range or specific IPs
+SERVER_IP_RANGE = '192.168.1.25'  # Should be a list or range for better flexibility
 
 captured_packets = []
 packet_count = 0
 cap_increment = 0
 
 def forward_packet(packet):
-    global captured_packets
-    global packet_count
-    global cap_increment
+    global captured_packets, packet_count, cap_increment
 
     if packet.haslayer(IP):
         original_ip = packet[IP]
@@ -34,30 +31,32 @@ def forward_packet(packet):
         
         if packet.haslayer(TCP):
             original_tcp = packet[TCP]
-            
+
+            if original_tcp.dport not in TARGET_PORT:
+                return
+
             new_packet = IP(src=original_ip.src, dst=original_ip.dst) / TCP(
                 sport=original_tcp.sport, dport=original_tcp.dport, flags=original_tcp.flags,
                 seq=original_tcp.seq, ack=original_tcp.ack, dataofs=original_tcp.dataofs, reserved=original_tcp.reserved,
                 window=original_tcp.window, chksum=original_tcp.chksum, urgptr=original_tcp.urgptr, options=original_tcp.options
             ) / original_tcp.payload
                 
-                
         elif packet.haslayer(UDP):
             original_udp = packet[UDP]
+
+            if original_udp.dport not in TARGET_PORT:
+                return
             
             new_packet = IP(src=original_ip.src, dst=original_ip.dst) / UDP(
                 sport=original_udp.sport, dport=original_udp.dport, len=original_udp.len, chksum=original_udp.chksum
             ) / original_udp.payload
 
-        if original_ip.src in SERVER_IP_RANGE:
-            # jangan log ketika ada trafic dari server
-            pass
-        else :
+        if original_ip.dst == SERVER_IP_RANGE:
             captured_packets.append(packet)
             packet_count += 1
 
-        if packet_count >= 10 :
-            wrpcap("client_traffic-" + cap_increment + ".pcap", captured_packets)
+        if packet_count >= 10:
+            wrpcap(f"./log/client_traffic-{cap_increment}.pcap", captured_packets)
             cap_increment += 1
             packet_count = 0
             captured_packets.clear()

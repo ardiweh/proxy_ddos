3ee8cdb552b6d398a03fe042f74b6aa8fbcb42d1
ardiweh <113125338+ardiweh@users.noreply.github.com>
2024-07-15 18:30:48 +0700

    Update proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -1,8 +1,7 @@
-from scapy.all import sniff, IP, TCP, UDP, send
+from scapy.all import sniff, send, IP, TCP, UDP, wrpcap
 
-# Define the target IP and ports mapping for forwarding the packets
-TARGET_IP = '192.168.1.100'
-TARGET_PORT_MAPPING = { # gak dipake
+# Define ports mapping for forwarding the packets
+TARGET_PORT_MAPPING = {
     80: 8080,   # HTTP
     443: 8443,  # HTTPS
     53: 8053,   # DNS
@@ -16,28 +15,56 @@ TARGET_PORT_MAPPING = { # gak dipake
     3389: 8389  # RDP
 }
 
+# Define the client and server IP address ranges or specific IPs
+# CLIENT_IP_RANGE = '192.168.1.0/24'
+SERVER_IP_RANGE = '192.168.1.25'
+
+captured_packets = []
+packet_count = 0
+cap_increment = 0
+
 def forward_packet(packet):
+    global captured_packets
+    global packet_count
+    global cap_increment
+
     if packet.haslayer(IP):
         original_ip = packet[IP]
         new_packet = None
         
         if packet.haslayer(TCP):
             original_tcp = packet[TCP]
-            new_packet = IP(src=original_ip.src, dst=TARGET_IP) / TCP(
+            
+            new_packet = IP(src=original_ip.src, dst=original_ip.dst) / TCP(
                 sport=original_tcp.sport, dport=original_tcp.dport, flags=original_tcp.flags,
                 seq=original_tcp.seq, ack=original_tcp.ack, dataofs=original_tcp.dataofs, reserved=original_tcp.reserved,
                 window=original_tcp.window, chksum=original_tcp.chksum, urgptr=original_tcp.urgptr, options=original_tcp.options
             ) / original_tcp.payload
                 
+                
         elif packet.haslayer(UDP):
             original_udp = packet[UDP]
-            new_packet = IP(src=original_ip.src, dst=TARGET_IP) / UDP(
+            
+            new_packet = IP(src=original_ip.src, dst=original_ip.dst) / UDP(
                 sport=original_udp.sport, dport=original_udp.dport, len=original_udp.len, chksum=original_udp.chksum
             ) / original_udp.payload
 
+        if original_ip.src in SERVER_IP_RANGE:
+            # jangan log ketika ada trafic dari server
+            pass
+        else :
+            captured_packets.append(packet)
+            packet_count += 1
+
+        if packet_count >= 10 :
+            wrpcap("client_traffic-" + cap_increment + ".pcap", captured_packets)
+            cap_increment += 1
+            packet_count = 0
+            captured_packets.clear()
+
         if new_packet:
             send(new_packet, verbose=False)
-            print(f"Forwarded {packet.summary()} from {original_ip.src}:{original_tcp.sport if packet.haslayer(TCP) else original_udp.sport} to {TARGET_IP}:{original_tcp.dport if packet.haslayer(TCP) else original_udp.dport}")
+            print(f"Forwarded {packet.summary()} from {original_ip.src}:{original_tcp.sport if packet.haslayer(TCP) else original_udp.sport} to {new_packet[IP].dst}:{new_packet[TCP].dport if packet.haslayer(TCP) else new_packet[UDP].dport}")
 
 # Start sniffing and forward packets
 sniff(filter="tcp or udp", prn=forward_packet)

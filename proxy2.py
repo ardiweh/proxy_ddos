5f30bb0f43999ae93c7e05bf9f2d60d6eac69060
ardiweh <113125338+ardiweh@users.noreply.github.com>
2024-07-15 19:05:25 +0700

    Update proxy2.py

--- proxy2.py
+++ proxy2.py
@@ -1,6 +1,6 @@
 from scapy.all import sniff, send, IP, TCP, UDP, wrpcap
 
-# Define ports mapping for forwarding the packets
+# Definisi port yang diinginkan untuk forwarding
 TARGET_PORT = {
     80,   # HTTP
     443,  # HTTPS
@@ -15,12 +15,16 @@ TARGET_PORT = {
     3389  # RDP
 }
 
-# Define the server IP address range or specific IPs
-SERVER_IP_RANGE = '192.168.1.25'  # Should be a list or range for better flexibility
+# Definisi alamat IP server
+SERVER_IP = '192.168.1.25'
 
 captured_packets = []
 packet_count = 0
 cap_increment = 0
+
+# Fungsi untuk memeriksa apakah IP adalah multicast atau broadcast
+def is_multicast_or_broadcast(ip):
+    return ip.startswith('224.') or ip.startswith('239.') or ip == '255.255.255.255'
 
 def forward_packet(packet):
     global captured_packets, packet_count, cap_increment
@@ -28,6 +32,10 @@ def forward_packet(packet):
     if packet.haslayer(IP):
         original_ip = packet[IP]
         new_packet = None
+        
+        # Tambahkan pengecekan apakah IP tujuan adalah multicast atau broadcast
+        if is_multicast_or_broadcast(original_ip.dst):
+            return
         
         if packet.haslayer(TCP):
             original_tcp = packet[TCP]
@@ -51,7 +59,8 @@ def forward_packet(packet):
                 sport=original_udp.sport, dport=original_udp.dport, len=original_udp.len, chksum=original_udp.chksum
             ) / original_udp.payload
 
-        if original_ip.dst == SERVER_IP_RANGE:
+        # Periksa apakah IP tujuan adalah SERVER_IP
+        if original_ip.dst == SERVER_IP:
             captured_packets.append(packet)
             packet_count += 1
 
@@ -65,5 +74,5 @@ def forward_packet(packet):
             send(new_packet, verbose=False)
             print(f"Forwarded {packet.summary()} from {original_ip.src}:{original_tcp.sport if packet.haslayer(TCP) else original_udp.sport} to {new_packet[IP].dst}:{new_packet[TCP].dport if packet.haslayer(TCP) else new_packet[UDP].dport}")
 
-# Start sniffing and forward packets
+# Mulai menangkap dan meneruskan paket
 sniff(filter="tcp or udp", prn=forward_packet)

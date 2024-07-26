05f858c660f3b7547bd15fd903f8d9c9b3ff46e1
ardiweh <113125338+ardiweh@users.noreply.github.com>
2024-07-27 01:00:50 +0700

    Update proxy7.py

--- proxy7.py
+++ proxy7.py
@@ -1,8 +1,4 @@
 from scapy.all import sniff, send, IP, TCP, UDP
-import os
-import threading
-import time
-import csv
 import socket
 
 # Definisi port yang diinginkan untuk forwarding
@@ -20,16 +16,10 @@ TARGET_PORT = {
     3389  # RDP
 }
 
-# Definisi alamat IP server
+# Definisi alamat IP server dan proxy
 SERVER_IP = '192.168.1.9'  # Alamat IP server yang menjalankan kode server
-SERVER_PORT = 12345         # Port yang digunakan server untuk menerima paket
-PROXY_IP = '192.168.1.10'   # Alamat IP dari proxy itu sendiri
-CAPTURED_PACKET_DIR = "./log"
-
-captured_data = []
-packet_count = 0
-cap_increment = 0
-lock = threading.Lock()
+SERVER_PORT = 12345        # Port yang digunakan server untuk menerima paket
+PROXY_IP = '192.168.1.10'  # Alamat IP dari proxy itu sendiri
 
 # Fungsi untuk memeriksa apakah IP adalah multicast atau broadcast
 def is_multicast_or_broadcast(ip):
@@ -45,11 +35,8 @@ def forward_packet_to_server(packet):
         print(f"Error sending packet to server: {e}")
 
 def forward_packet(packet):
-    global captured_data, packet_count, cap_increment
-
     if packet.haslayer(IP):
         original_ip = packet[IP]
-        new_packet = None
         
         # Tambahkan pengecekan apakah IP tujuan adalah multicast atau broadcast
         if is_multicast_or_broadcast(original_ip.dst):
@@ -63,62 +50,21 @@ def forward_packet(packet):
 
         if packet.haslayer(TCP):
             original_tcp = packet[TCP]
-
             if original_tcp.dport not in TARGET_PORT:
                 print(f"Skipping packet with dport not in TARGET_PORT: {original_tcp.dport}")
                 return
 
-            new_packet = IP(src=original_ip.src, dst=SERVER_IP) / TCP(
-                sport=original_tcp.sport, dport=original_tcp.dport, flags=original_tcp.flags,
-                seq=original_tcp.seq, ack=original_tcp.ack, dataofs=original_tcp.dataofs, reserved=original_tcp.reserved,
-                window=original_tcp.window, chksum=original_tcp.chksum, urgptr=original_tcp.urgptr, options=original_tcp.options
-            ) / original_tcp.payload
-                
         elif packet.haslayer(UDP):
             original_udp = packet[UDP]
-
             if original_udp.dport not in TARGET_PORT:
                 print(f"Skipping packet with dport not in TARGET_PORT: {original_udp.dport}")
                 return
-            
-            new_packet = IP(src=original_ip.src, dst=SERVER_IP) / UDP(
-                sport=original_udp.sport, dport=original_udp.dport, len=original_udp.len, chksum=original_udp.chksum
-            ) / original_udp.payload
 
-        if new_packet:
-            try:
-                forward_packet_to_server(new_packet)
-                print(f"Forwarded {packet.summary()} from {original_ip.src}:{original_tcp.sport if packet.haslayer(TCP) else original_udp.sport} to {SERVER_IP}:{SERVER_PORT}")
-            except Exception as e:
-                print(f"Error forwarding packet to server: {e}")
-
-def write_logs():
-    global captured_data, packet_count, cap_increment
-    while True:
-        time.sleep(1)
-        with lock:
-            if packet_count > 0:
-                try:
-                    # Pastikan direktori ada dan memiliki izin menulis
-                    if not os.path.exists(CAPTURED_PACKET_DIR):
-                        os.makedirs(CAPTURED_PACKET_DIR)
-                    file_path = os.path.join(CAPTURED_PACKET_DIR, f"client_traffic-{cap_increment}.csv")
-                    with open(file_path, 'w', newline='') as csvfile:
-                        fieldnames = list(captured_data[0].keys())
-                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                        writer.writeheader()
-                        for data in captured_data:
-                            writer.writerow(data)
-                    cap_increment += 1
-                    packet_count = 0
-                    captured_data.clear()
-                except Exception as e:
-                    print(f"Error writing to CSV file: {e}")
-
-# Memulai thread untuk menulis log
-log_thread = threading.Thread(target=write_logs)
-log_thread.daemon = True
-log_thread.start()
+        try:
+            forward_packet_to_server(packet)
+            print(f"Forwarded {packet.summary()} from {original_ip.src} to {SERVER_IP}:{SERVER_PORT}")
+        except Exception as e:
+            print(f"Error forwarding packet to server: {e}")
 
 # Mulai menangkap dan meneruskan paket
 sniff(filter="tcp or udp", prn=forward_packet)

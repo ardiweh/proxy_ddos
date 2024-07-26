2a63907dfb75981f1432ec8cab45822a6e1dd3c6
ardiweh <113125338+ardiweh@users.noreply.github.com>
2024-07-27 02:03:49 +0700

    Update server2.py

--- server2.py
+++ server2.py
@@ -2,9 +2,9 @@ import socket
 import joblib
 import pandas as pd
 import numpy as np
-from scapy.all import IP, UDP, TCP
 import requests
 from datetime import datetime
+import json
 
 # Load the trained model, PCA, and scaler
 model = joblib.load('best_random_forest_model.pkl')
@@ -61,87 +61,9 @@ def send_telegram_message(message):
         print(f"Failed to send message to Telegram: {e}")
 
 # Function to process packets
-def process_packet(packet):
-    print("Processing packet...")
-    packet_info = {
-        "Protocol": packet["Protocol"],
-        "Flow Duration": packet["Flow Duration"],
-        "Total Fwd Packets": packet["Total Fwd Packets"],
-        "Total Backward Packets": packet["Total Backward Packets"],
-        "Fwd Packets Length Total": packet["Fwd Packets Length Total"],
-        "Bwd Packets Length Total": packet["Bwd Packets Length Total"],
-        "Fwd Packet Length Max": packet["Fwd Packet Length Max"],
-        "Fwd Packet Length Min": packet["Fwd Packet Length Min"],
-        "Fwd Packet Length Mean": packet["Fwd Packet Length Mean"],
-        "Fwd Packet Length Std": packet["Fwd Packet Length Std"],
-        "Bwd Packet Length Max": packet["Bwd Packet Length Max"],
-        "Bwd Packet Length Min": packet["Bwd Packet Length Min"],
-        "Bwd Packet Length Mean": packet["Bwd Packet Length Mean"],
-        "Bwd Packet Length Std": packet["Bwd Packet Length Std"],
-        "Flow Bytes/s": packet["Flow Bytes/s"],
-        "Flow Packets/s": packet["Flow Packets/s"],
-        "Flow IAT Mean": packet["Flow IAT Mean"],
-        "Flow IAT Std": packet["Flow IAT Std"],
-        "Flow IAT Max": packet["Flow IAT Max"],
-        "Flow IAT Min": packet["Flow IAT Min"],
-        "Fwd IAT Total": packet["Fwd IAT Total"],
-        "Fwd IAT Mean": packet["Fwd IAT Mean"],
-        "Fwd IAT Std": packet["Fwd IAT Std"],
-        "Fwd IAT Max": packet["Fwd IAT Max"],
-        "Fwd IAT Min": packet["Fwd IAT Min"],
-        "Bwd IAT Total": packet["Bwd IAT Total"],
-        "Bwd IAT Mean": packet["Bwd IAT Mean"],
-        "Bwd IAT Std": packet["Bwd IAT Std"],
-        "Bwd IAT Max": packet["Bwd IAT Max"],
-        "Bwd IAT Min": packet["Bwd IAT Min"],
-        "Fwd PSH Flags": packet["Fwd PSH Flags"],
-        "Bwd PSH Flags": packet["Bwd PSH Flags"],
-        "Fwd URG Flags": packet["Fwd URG Flags"],
-        "BWD URG Flags": packet["BWD URG Flags"],
-        "Fwd Header Length": packet["Fwd Header Length"],
-        "Bwd Header Length": packet["Bwd Header Length"],
-        "Fwd Packets/s": packet["Fwd Packets/s"],
-        "Bwd Packets/s": packet["Bwd Packets/s"],
-        "Packet Length Min": packet["Packet Length Min"],
-        "Packet Length Max": packet["Packet Length Max"],
-        "Packet Length Mean": packet["Packet Length Mean"],
-        "Packet Length Std": packet["Packet Length Std"],
-        "Packet Length Variance": packet["Packet Length Variance"],
-        "FIN Flag Count": packet["FIN Flag Count"],
-        "SYN Flag Count": packet["SYN Flag Count"],
-        "RST Flag Count": packet["RST Flag Count"],
-        "PSH Flag Count": packet["PSH Flag Count"],
-        "ACK Flag Count": packet["ACK Flag Count"],
-        "URG Flag Count": packet["URG Flag Count"],
-        "CWE Flag Count": packet["CWE Flag Count"],
-        "ECE Flag Count": packet["ECE Flag Count"],
-        "Down/Up Ratio": packet["Down/Up Ratio"],
-        "Avg Packet Size": packet["Avg Packet Size"],
-        "Avg Fwd Segment Size": packet["Avg Fwd Segment Size"],
-        "Avg Bwd Segment Size": packet["Avg Bwd Segment Size"],
-        "Fwd Avg Bytes/Bulk": packet["Fwd Avg Bytes/Bulk"],
-        "Fwd Avg Packets/Bulk": packet["Fwd Avg Packets/Bulk"],
-        "Fwd Avg Bulk Rate": packet["Fwd Avg Bulk Rate"],
-        "Bwd Avg Bytes/Bulk": packet["Bwd Avg Bytes/Bulk"],
-        "Bwd Avg Packets/Bulk": packet["Bwd Avg Packets/Bulk"],
-        "Bwd Avg Bulk Rate": packet["Bwd Avg Bulk Rate"],
-        "Subflow Fwd Packets": packet["Subflow Fwd Packets"],
-        "Subflow Fwd Bytes": packet["Subflow Fwd Bytes"],
-        "Subflow Bwd Packets": packet["Subflow Bwd Packets"],
-        "Subflow Bwd Bytes": packet["Subflow Bwd Bytes"],
-        "Init Fwd Win Bytes": packet["Init Fwd Win Bytes"],
-        "Init Bwd Win Bytes": packet["Init Bwd Win Bytes"],
-        "Fwd Act Data Packets": packet["Fwd Act Data Packets"],
-        "Fwd Seg Size Min": packet["Fwd Seg Size Min"],
-        "Active Mean": packet["Active Mean"],
-        "Active Std": packet["Active Std"],
-        "Active Max": packet["Active Max"],
-        "Active Min": packet["Active Min"],
-        "Idle Mean": packet["Idle Mean"],
-        "Idle Std": packet["Idle Std"],
-        "Idle Max": packet["Idle Max"],
-        "Idle Min": packet["Idle Min"]
-    }
+def process_packet(data):
+    packet_info = json.loads(data.decode('utf-8'))
+    print(f"Packet info: {packet_info}")
 
     packet_df = pd.DataFrame([packet_info], columns=feature_names)
 
@@ -174,11 +96,7 @@ def udp_receiver():
     while True:
         data, addr = sock.recvfrom(65535)
         print(f"Received packet from {addr}")
-        try:
-            packet = eval(data.decode('utf-8'))
-            process_packet(packet)
-        except Exception as e:
-            print(f"Error processing packet: {e}")
+        process_packet(data)
 
 # Mulai menerima dan memproses paket UDP
 udp_receiver()
